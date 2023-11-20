4cc7ac9b0600410105a0cf6b5d8f1145f34600ca
gisellepvp <108085887+Gisellepvp@users.noreply.github.com>
2023-11-20 11:45:30 -0300

    calcu

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-let expression ='';
-
+let expression = '';
 function appendNumber(number) {
-    expression += number;
-    document.getElementById('result').value =expression;
+expression += number;
+document.getElementById('result').value = expression;
 }
-
-function appedOperstor(operator) {
-    expression += operator;
-    document.getElementById('result').value = expression;   
+function appedOperator(operator) {
+expression += operator;
+document.getElementById('result').value = expression;   
 }
-
 function calculate() {
-    try {
-        const result = eval(expression);
-        document.getElementById('result').value = result;
-        expression = '';
-    } catch (error) {
-      document.getElementById('result').value = 'Error';
-      expression = '';
-    }
+try {
+const result = eval(expression);
+document.getElementById('result').value = result;
+expression = '';
+} catch (error) {
+document.getElementById('result').value = 'Error';
+expression = '';
 }
-
 function clear() {
-    expression = '' ;
-    document.getElementById('result').value = '';
+expression = '' ;
+document.getElementById('result').value = '';
+}
 }
